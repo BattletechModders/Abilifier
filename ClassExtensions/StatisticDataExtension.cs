a86d7e6e406002a4f6ebc421f7f8be283cd49f9b
ajkroeg <50463892+ajkroeg@users.noreply.github.com>
2022-01-08 14:18:18 -0500

    Update StatisticDataExtension.cs

--- ClassExtensions/StatisticDataExtension.cs
+++ ClassExtensions/StatisticDataExtension.cs
@@ -50,152 +50,179 @@ namespace Abilifier.Patches
     }
 
     public static class EffectDataExtensions
+    {
+        public static EffectDataExtensionManager.EffectDataExtension getStatDataExtension(this EffectData statData)
         {
-            public static EffectDataExtensionManager.EffectDataExtension getStatDataExtension(this EffectData statData)
+            return EffectDataExtensionManager.ManagerInstance.ExtendedEffectDataDict.ContainsKey(
+                statData.Description.Id)
+                ? EffectDataExtensionManager.ManagerInstance.ExtendedEffectDataDict[statData.Description.Id]
+                : new EffectDataExtensionManager.EffectDataExtension();
+        }
+
+        public static List<MechComponent> GetTargetComponentsMatchingTags(ICombatant target,
+            StatisticEffectData.TargetCollection targetCollection, WeaponSubType weaponSubType, WeaponType weaponType,
+            WeaponCategoryValue weaponCategoryValue, AmmoCategoryValue ammoCategoryValue, TagSet tagSet)
+        {
+            List<MechComponent> list = new List<MechComponent>();
+            if (targetCollection == StatisticEffectData.TargetCollection.SingleRandomWeapon)
             {
-                return EffectDataExtensionManager.ManagerInstance.ExtendedEffectDataDict.ContainsKey(statData.Description.Id)
-                    ? EffectDataExtensionManager.ManagerInstance.ExtendedEffectDataDict[statData.Description.Id]
-                    : new EffectDataExtensionManager.EffectDataExtension();
+                AbstractActor abstractActor = target as AbstractActor;
+                if (abstractActor != null)
+                {
+                    List<Weapon> list2 = abstractActor.Weapons.FindAll((Weapon x) =>
+                        !x.IsDisabled && tagSet.Overlaps(x.componentDef.ComponentTags));
+                    if (list2.Count > 0)
+                    {
+                        list.Add(list2.GetRandomElement());
+                    }
+                }
+            }
+            else if (targetCollection == StatisticEffectData.TargetCollection.StrongestWeapon)
+            {
+                AbstractActor abstractActor2 = target as AbstractActor;
+                if (abstractActor2 != null)
+                {
+                    List<Weapon> list3 = abstractActor2.Weapons.FindAll((Weapon x) =>
+                        !x.IsDisabled && tagSet.Overlaps(x.componentDef.ComponentTags));
+                    if (list3.Count > 0)
+                    {
+                        list3.Sort((Weapon a, Weapon b) =>
+                            b.DamagePerShot.CompareTo(a.DamagePerShot * (float) a.ShotsWhenFired));
+                        list.Add(list3[0]);
+                    }
+                }
+            }
+            else if (targetCollection == StatisticEffectData.TargetCollection.Weapon)
+            {
+                AbstractActor abstractActor3 = target as AbstractActor;
+                if (abstractActor3 != null)
+                {
+                    List<Weapon> list4 = new List<Weapon>();
+                    if (weaponSubType != WeaponSubType.NotSet)
+                    {
+                        if (weaponSubType == WeaponSubType.Melee)
+                        {
+                            Mech mech = abstractActor3 as Mech;
+                            if (mech != null)
+                            {
+                                list4.Add(mech.MeleeWeapon);
+                            }
+                        }
+                        else if (weaponSubType == WeaponSubType.DFA)
+                        {
+                            Mech mech2 = abstractActor3 as Mech;
+                            if (mech2 != null)
+                            {
+                                list4.Add(mech2.DFAWeapon);
+                            }
+                        }
+                        else
+                        {
+                            list4 = abstractActor3.Weapons.FindAll((Weapon x) =>
+                                x.WeaponSubType == weaponSubType && tagSet.Overlaps(x.componentDef.ComponentTags));
+                        }
+                    }
+                    else if (weaponType != WeaponType.NotSet)
+                    {
+                        list4 = abstractActor3.Weapons.FindAll((Weapon x) =>
+                            x.Type == weaponType && tagSet.Overlaps(x.componentDef.ComponentTags));
+                    }
+                    else if (!weaponCategoryValue.Is_NotSet)
+                    {
+                        list4 = abstractActor3.Weapons.FindAll((Weapon x) =>
+                            x.WeaponCategoryValue.ID == weaponCategoryValue.ID &&
+                            tagSet.Overlaps(x.componentDef.ComponentTags));
+                    }
+                    else
+                    {
+                        list4 = new List<Weapon>(
+                            abstractActor3.Weapons.FindAll(x => tagSet.Overlaps(x.componentDef.ComponentTags)));
+                    }
+
+                    for (int i = 0; i < list4.Count; i++)
+                    {
+                        list.Add(list4[i]);
+                    }
+                }
+            }
+            else if (targetCollection == StatisticEffectData.TargetCollection.AmmoBox)
+            {
+                AbstractActor abstractActor4 = target as AbstractActor;
+                if (abstractActor4 != null)
+                {
+                    List<AmmunitionBox> list5 = new List<AmmunitionBox>();
+                    if (!ammoCategoryValue.Is_NotSet)
+                    {
+                        list5 = abstractActor4.ammoBoxes.FindAll((AmmunitionBox x) =>
+                            x.ammoCategoryValue.Equals(ammoCategoryValue) &&
+                            tagSet.Overlaps(x.componentDef.ComponentTags));
+                    }
+                    else
+                    {
+                        list5 = new List<AmmunitionBox>(
+                            abstractActor4.ammoBoxes.FindAll(x => tagSet.Overlaps(x.componentDef.ComponentTags)));
+                    }
+
+                    for (int j = 0; j < list5.Count; j++)
+                    {
+                        list.Add(list5[j]);
+                    }
+                }
             }
 
-            public static List<MechComponent> GetTargetComponentsMatchingTags(ICombatant target,
-                StatisticEffectData.TargetCollection targetCollection, WeaponSubType weaponSubType, WeaponType weaponType,
-                WeaponCategoryValue weaponCategoryValue, AmmoCategoryValue ammoCategoryValue, TagSet tagSet)
+            return list;
+        }
+
+        public class EffectDataExtensionPatches
+        {
+            [HarmonyPatch(typeof(EffectManager), "GetTargetStatCollections")]
+            public static class EffectManager_GetTargetStatCollections
             {
-                List<MechComponent> list = new List<MechComponent>();
-                if (targetCollection == StatisticEffectData.TargetCollection.SingleRandomWeapon)
+                public static bool Prefix(EffectManager __instance, EffectData effectData, ICombatant target,
+                    ref List<StatCollection> __result)
                 {
-                    AbstractActor abstractActor = target as AbstractActor;
-                    if (abstractActor != null)
+                    List<StatCollection> list = new List<StatCollection>();
+                    StatisticEffectData.TargetCollection targetCollection = effectData.statisticData.targetCollection;
+                    WeaponSubType targetWeaponSubType = effectData.statisticData.targetWeaponSubType;
+                    WeaponType targetWeaponType = effectData.statisticData.targetWeaponType;
+                    WeaponCategoryValue targetWeaponCategoryValue = effectData.statisticData.TargetWeaponCategoryValue;
+                    AmmoCategoryValue targetAmmoCategoryValue = effectData.statisticData.TargetAmmoCategoryValue;
+                    if (targetCollection == StatisticEffectData.TargetCollection.NotSet)
                     {
-                        List<Weapon> list2 = abstractActor.Weapons.FindAll((Weapon x) => !x.IsDisabled && tagSet.Overlaps(x.componentDef.ComponentTags));
-                        if (list2.Count > 0)
-                        {
-                            list.Add(list2.GetRandomElement());
-                        }
+                        return true;
                     }
-                }
-                else if (targetCollection == StatisticEffectData.TargetCollection.StrongestWeapon)
-                {
-                    AbstractActor abstractActor2 = target as AbstractActor;
-                    if (abstractActor2 != null)
-                    {
-                        List<Weapon> list3 = abstractActor2.Weapons.FindAll((Weapon x) => !x.IsDisabled && tagSet.Overlaps(x.componentDef.ComponentTags));
-                        if (list3.Count > 0)
-                        {
-                            list3.Sort((Weapon a, Weapon b) => b.DamagePerShot.CompareTo(a.DamagePerShot * (float)a.ShotsWhenFired));
-                            list.Add(list3[0]);
-                        }
-                    }
-                }
-                else if (targetCollection == StatisticEffectData.TargetCollection.Weapon)
-                {
-                    AbstractActor abstractActor3 = target as AbstractActor;
-                    if (abstractActor3 != null)
-                    {
-                        List<Weapon> list4 = new List<Weapon>();
-                        if (weaponSubType != WeaponSubType.NotSet)
-                        {
-                            if (weaponSubType == WeaponSubType.Melee)
-                            {
-                                Mech mech = abstractActor3 as Mech;
-                                if (mech != null)
-                                {
-                                    list4.Add(mech.MeleeWeapon);
-                                }
-                            }
-                            else if (weaponSubType == WeaponSubType.DFA)
-                            {
-                                Mech mech2 = abstractActor3 as Mech;
-                                if (mech2 != null)
-                                {
-                                    list4.Add(mech2.DFAWeapon);
-                                }
-                            }
-                            else
-                            {
-                                list4 = abstractActor3.Weapons.FindAll((Weapon x) => x.WeaponSubType == weaponSubType && tagSet.Overlaps(x.componentDef.ComponentTags));
-                            }
-                        }
-                        else if (weaponType != WeaponType.NotSet)
-                        {
-                            list4 = abstractActor3.Weapons.FindAll((Weapon x) => x.Type == weaponType && tagSet.Overlaps(x.componentDef.ComponentTags));
-                        }
-                        else if (!weaponCategoryValue.Is_NotSet)
-                        {
-                            list4 = abstractActor3.Weapons.FindAll((Weapon x) => x.WeaponCategoryValue.ID == weaponCategoryValue.ID && tagSet.Overlaps(x.componentDef.ComponentTags));
-                        }
-                        else
-                        {
-                            list4 = new List<Weapon>(abstractActor3.Weapons.FindAll(x=>tagSet.Overlaps(x.componentDef.ComponentTags)));
-                        }
-                        for (int i = 0; i < list4.Count; i++)
-                        {
-                            list.Add(list4[i]);
-                        }
-                    }
-                }
-                else if (targetCollection == StatisticEffectData.TargetCollection.AmmoBox)
-                {
-                    AbstractActor abstractActor4 = target as AbstractActor;
-                    if (abstractActor4 != null)
-                    {
-                        List<AmmunitionBox> list5 = new List<AmmunitionBox>();
-                        if (!ammoCategoryValue.Is_NotSet)
-                        {
-                            list5 = abstractActor4.ammoBoxes.FindAll((AmmunitionBox x) => x.ammoCategoryValue.Equals(ammoCategoryValue) && tagSet.Overlaps(x.componentDef.ComponentTags));
-                        }
-                        else
-                        {
-                            list5 = new List<AmmunitionBox>(abstractActor4.ammoBoxes.FindAll(x=>tagSet.Overlaps(x.componentDef.ComponentTags)));
-                        }
-                        for (int j = 0; j < list5.Count; j++)
-                        {
-                            list.Add(list5[j]);
-                        }
-                    }
-                }
-                return list;
-            }
 
-            public class EffectDataExtensionPatches
-            {
-               [HarmonyPatch(typeof(EffectManager), "GetTargetStatCollections")]
-                public static class EffectManager_GetTargetStatCollections
-                {
-                    public static bool Prefix(EffectManager __instance, EffectData effectData, ICombatant target,
-                        ref List<StatCollection> __result)
+                    if (effectData.getStatDataExtension().TargetComponentTagMatch.Count <= 0)
                     {
-                        List<StatCollection> list = new List<StatCollection>();
-                        StatisticEffectData.TargetCollection targetCollection = effectData.statisticData.targetCollection;
-                        WeaponSubType targetWeaponSubType = effectData.statisticData.targetWeaponSubType;
-                        WeaponType targetWeaponType = effectData.statisticData.targetWeaponType;
-                        WeaponCategoryValue targetWeaponCategoryValue = effectData.statisticData.TargetWeaponCategoryValue;
-                        AmmoCategoryValue targetAmmoCategoryValue = effectData.statisticData.TargetAmmoCategoryValue;
-                        if (targetCollection == StatisticEffectData.TargetCollection.NotSet ||
-                            targetCollection == StatisticEffectData.TargetCollection.Pilot)
-                        {
-                            return true;
-                        }
-
-                        if (effectData.getStatDataExtension().TargetComponentTagMatch.Count <= 0)
-                        {
-                            return true;
-                        }
-                        else
-                        {
-                            List<MechComponent> targetComponents = GetTargetComponentsMatchingTags(target, targetCollection,
-                                targetWeaponSubType, targetWeaponType, targetWeaponCategoryValue, targetAmmoCategoryValue, effectData.getStatDataExtension().TargetComponentTagMatch);
-                            for (int i = 0; i < targetComponents.Count; i++)
-                            {
-                                list.Add(targetComponents[i].StatCollection);
-                            }
-                        }
-
-                        __result = list;
-                        return false;
+                        return true;
                     }
+
+                    if (targetCollection == StatisticEffectData.TargetCollection.Pilot)
+                    {
+                        if (target.IsPilotable)
+                        {
+                            var pilot = target.GetPilot();
+                            if (effectData.getStatDataExtension().TargetComponentTagMatch
+                                .Overlaps(pilot.pilotDef.PilotTags))
+                                list.Add(target.GetPilot().StatCollection);
+                        }
+                    }
+
+                    else
+                    {
+                        List<MechComponent> targetComponents = GetTargetComponentsMatchingTags(target, targetCollection,
+                            targetWeaponSubType, targetWeaponType, targetWeaponCategoryValue, targetAmmoCategoryValue,
+                            effectData.getStatDataExtension().TargetComponentTagMatch);
+                        for (int i = 0; i < targetComponents.Count; i++)
+                        {
+                            list.Add(targetComponents[i].StatCollection);
+                        }
+                    }
+
+                    __result = list;
+                    return false;
                 }
             }
         }
+    }
 }
